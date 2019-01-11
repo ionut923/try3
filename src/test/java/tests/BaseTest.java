e260f31d289fe19e334eeeebaec4a6d3c2133b66
bartamihai <mihai.barta@evozon.com>
2019-01-11 18:01:44 +0200

    base test committed

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -11,10 +11,10 @@ public class BaseTest {
 
 	@Before
 	public void setup() {
-		System.setProperty("http.proxyHost", "localhost");
-		System.setProperty("http.proxyPort", "8082");
-		System.setProperty("https.proxyHost", "localhost");
-		System.setProperty("https.proxyPort", "8082");
+//		System.setProperty("http.proxyHost", "localhost");
+//		System.setProperty("http.proxyPort", "8082");
+//		System.setProperty("https.proxyHost", "localhost");
+//		System.setProperty("https.proxyPort", "8082");
 		System.setProperty("env", "qa-env");
 	}
 }
